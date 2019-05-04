5fbbc41992f9a267e65cec2429bc0928a370960b
Marat <mataylnk@yandex.ru>
2019-05-04 12:22:08 +0700

    Это наш первый комит

---
+++ porject1/script.js
@@ -0,0 +1,34 @@
+'use strict'
+// коментарий 
+/*коментарий 
+*/
+console.log(4/0);
+console.log('string'*9 );
+let persone = {
+    name: "John", 
+    age:25,
+    isMarried:false
+};
+console.log(persone.name)
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[2]);
+//alert('Привет как дела?')
+
+//let answer = confirm("ТЫ хороший?");
+//console.log(answer);
+
+// let answer = +prompt("Сколько тебе лет?", "31");
+// console.log(typeof(answer));
+
+// console.log("arr"+"-object");
+
+let incr = 10,
+    decr = 10;
+
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
